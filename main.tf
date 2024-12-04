f400c77ba4e7ec81de439649efe2aedda3b48126
anumondal1 <163758779+anumondal1@users.noreply.github.com>
2024-12-04 14:45:11 -0500

    Updated Terraform files and workflow

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "azurerm_mssql_server" "sqlserver" {
   version                      = "12.0"
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-} 
+}
 
 // SQL Database
 
